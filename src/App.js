0904ea6aded218f3666993e4f9576974581b3309
ed18xy <75281079+ed18xy@users.noreply.github.com>
2022-01-25 14:35:51 -0500

    AnimationTutorial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import './App.css';
+import styles from './App.module.css';
+import React from 'react';
+//import background from './pics/skybgr.jpg';
+import spaceship from './pics/spaceship.png';
+import gsap from 'gsap';
+const { useEffect, useRef, forwardRef, useState } = React;
+
+const Box = forwardRef(({ children }, ref) => {
+  return <div className={styles.box} ref={ref}>{children}</div>;
+});
 
 function App() {
+  const [reversed, setReversed] = useState(false);
+  const boxy = useRef();
+  const tl = useRef();
+  useEffect(() => {            
+    tl.current = gsap.timeline()
+      .to(boxy.current, {
+        y: 150,
+        x: 100,
+        rotate: 180
+      })
+  }, []);
+  
+  useEffect(() => {
+    tl.current.reversed(reversed);    
+  }, [reversed]);
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          This is content 
-        </p>
-      </header>
+    <div className={styles.App} >
+      <div>
+        <button onClick={() => setReversed(!reversed)}>Toggle</button>
+      </div>
+      <Box ref={boxy}><img src={spaceship} alt='ship'></img></Box>
     </div>
   );
 }
