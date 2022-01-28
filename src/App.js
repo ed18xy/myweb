10202d7ff91c6de37eacc2cfcc2f09de297f1d2f
ed18xy <75281079+ed18xy@users.noreply.github.com>
2022-01-27 23:41:44 -0500

    path practice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import styles from './App.module.css';
 import React from 'react';
-//import background from './pics/skybgr.jpg';
 import spaceship from './pics/spaceship.png';
 import gsap from 'gsap';
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+gsap.registerPlugin(MotionPathPlugin);
 const { useEffect, useRef, forwardRef, useState } = React;
 
 const Box = forwardRef(({ children }, ref) => {
@@ -13,24 +14,35 @@ function App() {
   const [reversed, setReversed] = useState(false);
   const boxy = useRef();
   const tl = useRef();
-  useEffect(() => {            
+  useEffect(() => {
     tl.current = gsap.timeline()
       .to(boxy.current, {
-        y: 150,
-        x: 100,
-        rotate: 180
+        duration: 5,
+        repeat: 12,
+        repeatDelay: 3,
+        yoyo: true,
+        ease: "power1.inOut",
+        motionPath: {
+          path: "#path",
+          align: "#path",
+          autoRotate: true,
+          alignOrigin: [0.5, 0.5]
+        }
       })
   }, []);
-  
+
   useEffect(() => {
-    tl.current.reversed(reversed);    
+    tl.current.reversed(reversed);
   }, [reversed]);
-   
+
   return (
     <div className={styles.App} >
       <div>
         <button onClick={() => setReversed(!reversed)}>Toggle</button>
       </div>
+      <svg width="100%" height="100%" id="svg">
+        <path id="path" d="M 100 350 q 150 -300 300 0" />
+      </svg>
       <Box ref={boxy}><img src={spaceship} alt='ship'></img></Box>
     </div>
   );
